99de30097d3b32eb1f027afffa16dc3c004fee62
KIHAL Aya <kihalaya00@gmail.com>
2023-06-16 15:57:01 -0700

    Task completed - AYA -

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+/* Realized by : KIHAL Aya */
+
+
+#include <stdio.h>
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+/* Realized by : KIHAL Aya*/
+
+
+#include <stdio.h>
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
