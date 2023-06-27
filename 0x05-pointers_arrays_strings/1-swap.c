e2e6a08928e897a065c7069ec1c1c595940ffd2f
KIHAL Aya <kihalaya00@gmail.com>
2023-06-26 17:53:51 -0700

    Done -AYA-

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+/* Realized by : KIHAL Aya */
+/**
+ * swap_int - swaps the values of two integers
+ * @a: pointer to first value
+ * @b: pointer to second value
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
