56688473e290f8c710981c5db5a6e4db0c07bf79
KIHAL Aya <kihalaya00@gmail.com>
2023-07-28 06:58:04 -0700

    Task 0 Done _FILES ADDED

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+/* Author : KIHAL Aya */
+#include <stdio.h>
+#include "lists.h"
+
+
+/**
+ * print_list - Prints all the elements of a list_t list.
+ * @h: Pointer to the head of the linked list.
+ *
+ * Return: the number of nods (elements) in the list.
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+			printf("[%u] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
