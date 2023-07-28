0106071224ae3c83d10b0c9b4c4a11056c50920f
KIHAL Aya <kihalaya00@gmail.com>
2023-07-28 12:01:57 -0700

    Task 5 Done _FILES ADDED

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+/* Author : KIHAL Aya */
+#include <stdio.h>
+
+/**
+ * print_message - Function that prints a message before main is executed.
+ */
+void __attribute__((constructor)) print_message(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
+
