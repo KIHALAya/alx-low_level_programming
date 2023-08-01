0459a0643a55d3d87013a64321495d1958c3bc89
KIHAL Aya <kihalaya00@gmail.com>
2023-08-01 03:45:38 -0700

    Task 0 1 2 Done -FILES UPDATED -

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,6 +1,6 @@
 /* Author : KIHAL Aya */
+#include <stdio.h>
 #include "lists.h"
-
 /**
  * listint_len - returns the number of elemens in a linked listint_t list.
  * @h: pointer to the head of the list.
--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,4 +1,5 @@
 /* Author : KIHAL Aya */
+#include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
 
