b37469c7f9e6643125398c879e420c5dce0e8faa
KIHALAya <kihalaya00@gmail.com>
2023-08-31 14:06:58 -0700

    Task 2: adding files

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,24 @@
+/* Author : KIHAL Aya */
+#include "main.h"
+/**
+ * get_bit - Gets the value of a bit at a given index.
+ * @n: The number to extract the bit from.
+ * @index: The index of the bit to retrieve.
+ *
+ * Return: The value of the bit at the given index, or -1 if an error occurs.
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mask;
+
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
+	mask = (n >> index) & 1;
+
+	return (mask);
+
+
+}
+
