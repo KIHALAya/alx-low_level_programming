deac01998b2ba8d79a7af46c1b535f025bbb5a81
KIHALAya <kihalaya00@gmail.com>
2023-09-04 13:23:39 -0700

    Task 1 : adding files

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,57 @@
+/* Author : KIHAL Aya */
+#include "main.h"
+
+/**
+ * lenofstr - return the length of string.
+ * @string: string we desire determin the length of.
+ *
+ * Return: the length of string
+ */
+
+int lenofstr(char const *string)
+{
+	int length = 0;
+
+	while (string[length] != '\0')
+	{
+		length++;
+	}
+
+	return (length);
+}
+
+/**
+ * create_file - creates a file.
+ * @filename: name of the file to be created.
+ * @text_content: a NULL terminated string to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t write_result;
+
+		if (filename != NULL)
+		{
+			fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+			if (fd != -1)
+			{
+				if (text_content != NULL)
+				{
+					write_result = write(fd, text_content, lenofstr(text_content));
+					if (write_result != -1)
+					{
+						close(fd);
+						return (1);
+					}
+				}
+			}
+		}
+
+	close(fd);
+	return (1);
+}
+
+
+
