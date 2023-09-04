093eb3188f57ece72f5c9fd4da20d694f1691942
KIHALAya <kihalaya00@gmail.com>
2023-09-04 14:07:02 -0700

    my own strlen()

---
+++ 0x15-file_io/lenofstr.c
@@ -0,0 +1,19 @@
+/* Author : KIHAL Aya */
+/**
+ * lenofstr - return the length of string.
+ * @string: string we desire determin the length of.
+ *
+ * Return: the length of string
+ */
+int lenofstr(const char *string)
+{
+	int length = 0;
+
+	while (string[length] != '\0')
+	{
+		length++;
+	}
+
+	return (length);
+}
+
