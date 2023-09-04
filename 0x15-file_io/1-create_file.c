87b8e26dbbbbf030f3a1f1f6f28f592fe814f5b2
KIHALAya <kihalaya00@gmail.com>
2023-09-04 13:43:37 -0700

    Task 1 : debug ( handle errors properly )

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,8 +7,7 @@
  *
  * Return: the length of string
  */
-
-int lenofstr(char const *string)
+int lenofstr(const char *string)
 {
 	int length = 0;
 
@@ -32,26 +31,24 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	ssize_t write_result;
 
-	if (filename != NULL)
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
 	{
-		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-		if (fd != -1)
+		write_result = write(fd, text_content, lenofstr(text_content));
+		if (write_result == -1)
 		{
-			if (text_content != NULL)
-			{
-				write_result = write(fd, text_content, lenofstr(text_content));
-				if (write_result != -1)
-				{
-					close(fd);
-					return (1);
-				}
-			}
+			close(fd);
+			return (-1);
 		}
 	}
 
 	close(fd);
-	return (-1);
+	return (1);
 }
-
-
 
