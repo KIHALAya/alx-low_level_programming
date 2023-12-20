8e2ef584b835671bafd140dd43b267ad8559e0e4
KIHALAya <kihalaya00@gmail.com>
2023-12-20 15:16:38 -0800

    0x1C. C - Makefiles

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module to calculate the perimeter of the island in a given grid.
+"""
+
+
+def island_perimeter(grid):
+    """
+    Calculates the perimeter of the island in the grid.
+
+    Args:
+        grid (List[List[int]]): A list of lists representing the grid.
+
+    Returns:
+        int: The perimeter of the island.
+    """
+    perimeter = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                perimeter += 4
+
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+
+    return perimeter
